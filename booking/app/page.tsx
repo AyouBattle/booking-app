928eff016ecc0ef3da6fcd8a033a130cbcdc4890
AyouBattle <152877288+AyouBattle@users.noreply.github.com>
2025-05-13 23:40:13 +0200

    Second phase

--- booking/app/page.tsx
+++ booking/app/page.tsx
@@ -1,9 +1,21 @@
+import { Calendar } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <button className="btn">Test</button>
+    <div className="relative">
+      <section className="w-full">
+        <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
+          <div className="max-w-lg">
+            <h1>
+              <div>
+                <Calendar />
+              </div>
+              <span>book<span>It</span></span>
+            </h1>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
