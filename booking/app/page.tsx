266f6da68abb0ed9a58db532449f84f4fea071bb
AyouBattle <152877288+AyouBattle@users.noreply.github.com>
2025-05-15 20:03:22 +0200

    fix calander

--- booking/app/page.tsx
+++ booking/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <section className="w-full">
         <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
           <div className="max-w-lg">
-            <h1>
+            <h1 className="text-3x1 md:text-7x1">
               <div className="bg-secondary p-1 mr-1 rounded-md text-white">
                 <CalendarCheck  />
               </div>
